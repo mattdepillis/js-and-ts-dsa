eba41e6651f61a9663e17799c3d9cf5ad992d3f6
mattdepillis <mattdepillis@gmail.com>
2023-01-11 17:49:35 -0500

    initial commit; code for ae/arrayMethods myMap()

---
+++ ae/arrayMethods/solution.js
@@ -0,0 +1,26 @@
+/* 
+  Write three functions:
+    - myMap(), which should mimic Array.prototype.map(),
+    - myFilter(), which should mimic Array.prototype.filter(), and
+    - myReduce(), which should mimic Array.prototype.reduce()
+*/
+
+// * test array
+const array = [1, 2, 3, 4, 5]
+
+
+/**
+ * A function that mimics the native Array.prototype.map.
+ * @param {Function} callback The callback function to be applied on the array elements.
+ * @returns {Array} A new array containing the values at each index of the original array transformed by the callback function.
+*/
+Array.prototype.myMap = function(callback) {
+  const newArr = []
+  for (let i = 0; i < this.length; i++) {
+    newArr[i] = callback(this[i], i, this)
+  }
+
+  return newArr
+}
+
+console.log(array.myMap(i => i + 1))
