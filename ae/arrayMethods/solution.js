b71ad1f5dc2c87dc9d85957c7e9607718febdeda
mattdepillis <mattdepillis@gmail.com>
2023-01-12 12:47:58 -0500

    final js and ts solutions for arrayMethods - custom map, filter, reduce

--- ae/arrayMethods/solution.js
+++ ae/arrayMethods/solution.js
@@ -19,22 +19,36 @@ Array.prototype.myMap = function(callback) {
   for (let i = 0; i < this.length; i++) {
     newArr[i] = callback(this[i], i, this)
   }
-
   return newArr
 }
 
 /**
  * A function that mimics the native Array.prototype.filter.
- * @param
- * @returns
+ * @param {Function} callback The callback function to be applied on the array elements.
+ * @returns {Array} A new array containing the values at each index of the original array transformed by the callback function.
 */
 Array.prototype.myFilter = function(callback) {
-  newArr = []
+  const newArr = []
   for (let i = 0; i < this.length; i++) {
-    if (callback(this[i])) newArr.push(this[i])
+    if (callback(this[i], i, this) === true) newArr.push(this[i])
   }
   return newArr
 }
 
+/**
+ * A function that mimics the native Array.prototype.reduce.
+ * @param {Function} callback The callback function to be applied on the array elements.
+ * @param {any} initialValue the initial value of the accumulator
+ * @returns {any} The accumulated value of the accumulator initialValue.
+*/
+Array.prototype.myReduce = function(callback, initialValue) {
+  let start = (initialValue === undefined) ? 1 : 0
+  if (!initialValue && this.length > 0) initialValue = 0
+
+  for (let i = start; i < this.length; i++) initialValue = callback(initialValue, this[i], i, this)
+  return initialValue
+}
+
 // console.log(array.myMap(i => i + 1))
-console.log(array.myFilter(i => i % 2 == 0))
+// console.log(array.myFilter(i => i % 2 == 0))
+console.log(array.myReduce((acc, val) => acc + val, 0))
--- ae/arrayMethods/solution.ts
+++ ae/arrayMethods/solution.ts
@@ -14,6 +14,9 @@ interface Array<T> {
 
   /* myFilter */
   myFilter<U>(callbackfn: (value: number, i: number, array: T[]) => U, thisArg?: any): T[]
+
+  /* myReduce */
+  myReduce<U>(callbackfn: (acc: any, value: any, i: number, array: T[]) => U, thisArg?: any): any
 }
 
 // * test array
@@ -36,16 +39,31 @@ Array.prototype.myMap = function(callback) {
 
 /**
  * A function that mimics the native Array.prototype.filter.
- * @param
- * @returns
+ * @param {Function(number, number, T[])} callback The callback function to be applied on the array elements.
+ * @returns {Array<T>} A new array containing the values at each index of the original array transformed by the callback function.
 */
 Array.prototype.myFilter = function (callback) {
   const newArr: any[] = []
   for (let i = 0; i < this.length; i++) {
-    if (callback(this[i], i, this)) newArr.push(this[i])
+    if (callback(this[i], i, this) === true) newArr.push(this[i])
   }
   return newArr
 }
 
+/**
+ * A function that mimics the native Array.prototype.reduce.
+ * @param {Function} callback The callback function to be applied on the array elements.
+ * @param {any} initialValue the initial value of the accumulator
+ * @returns {any} The accumulated value of the accumulator initialValue.
+*/
+Array.prototype.myReduce = function(callback, initialValue) {
+  let start: number = (initialValue === undefined) ? 1 : 0
+  if (!initialValue && this.length > 0) initialValue = 0
+
+  for (let i = start; i < this.length; i++) initialValue = callback(initialValue, this[i], i, this)
+  return initialValue
+}
+
 console.log(arr.myMap((i: number): boolean => i == 2))
 console.log(arr.myFilter((i: number): boolean => i % 2 == 0))
+console.log(arr.myReduce(((i: number, val: number): number => i + val))) // 14
