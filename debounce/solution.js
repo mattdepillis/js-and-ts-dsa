c99d6475e4d455afa41d2a941e82c853a7f138ca
mattdepillis <mattdepillis@gmail.com>
2023-01-17 17:39:04 -0500

    full solution1 for debounce -- need comments + takeaways on  context

--- debounce/solution.js
+++ debounce/solution.js
@@ -1,25 +1,41 @@
 /*
 Write a custom debounce function.
 */
-const debounce = (callback, delay, immediate = false) => {
+function debounce(callback, delay, immediate = false) {
   this.timer = undefined
   this.executing_immediate = undefined
-  return((...args) => {
+  return function (...args) {
     if (immediate) {
-      if (Date.now() <= this.executing_immediate) return
+      const time_diff = this.executing_immediate - Date.now()
+      if (time_diff > 0) {
+        this.executing_immediate += delay - time_diff
+        return
+      }
       else {
         this.executing_immediate = Date.now() + delay
-        return callback.call(this, ...args)
+        return callback.apply(this, args)
       }
+    } else {
+      clearTimeout(this.timer)
+      this.timer = setTimeout(() => callback.apply(this, args), delay)
     }
-    clearTimeout(this.timer)
-    this.timer = setTimeout(() => callback.call(this, ...args), delay)
     return this.timer
-  })
+  }
 }
 
-const c = (w1, w2) => console.log(w1, w2)
+// const c = (w1, w2) => { console.log('called'); console.log(w1, w2); }
 
-const debounced = debounce(c, 1000, true)
-debounced('foo', 'bar')
-debounced('j', 'k')
+function spy () { return () => console.log('spy there!') }
+
+// const debounced = debounce(c, 1000, true)
+// debounced('foo', 'bar')
+// debounced('j', 'k')
+// debounced('l', 'm')
+
+const obj = {spy: spy()}
+function c() { this.spy() }
+
+obj.debounced = debounce(c, 1000)
+// obj.imm_debounced = debounce(c, 1000, true)
+obj.debounced()
+// obj.imm_debounced()
