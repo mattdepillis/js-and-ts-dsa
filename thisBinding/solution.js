19e4974987982ea5619d6e7c661c5cf4ee681278
mattdepillis <mattdepillis@gmail.com>
2023-01-19 20:16:05 -0500

    refactored methods  -- passed all tests; descriptive comments

--- thisBinding/solution.js
+++ thisBinding/solution.js
@@ -5,38 +5,54 @@ Implement methods on the Function prototype:
   - myBind (replicates native Function.prototype.bind)
 */
 
-Function.prototype.myCall = function(thisContext, ...args) {
-  // const s = Symbol()
-  // thisContext[s] = this
-  // val = thisContext[s](...args)
-  // delete thisContext[s]
-  // return val
-}
-
-// * starting with Function.prototype.myBind
+/**
+ * Binds a function to a passed-through thisContext.
+ * @param {Function} thisContext the item to be bound as the return function's `this` keyword
+ * @param {any} args the remaining arguments to be passed down to the callback
+ * @returns {Function} a new function that calls the original function with thisContext bound to the function's `this keyword
+*/
 Function.prototype.myBind = function(thisContext, ...args) {
-  const s = Symbol()
-  thisContext[s] = this
-  initArgs = args
-  return (...args) => thisContext[s](...(initArgs + args))
+  return (...extraArgs) => {
+    const s = Symbol()
+    thisContext[s] = this
+    const rv = thisContext[s](...args, ...extraArgs)
+    delete thisContext[s]
+    return rv
+  }
 }
 
+/**
+ * Applies an object as a thisContext and additional arguments to the callback function. Args are applied by passing through an array of arguments.
+ * @param {Function} thisContext the item to be bound as the return function's `this` keyword
+ * @param {any} args the remaining arguments to be passed down to the callback
+ * @returns {any} the return value of the callback function, given thisContext and args
+*/
+Function.prototype.myApply = function(thisContext, args = []) {
+  return this.myBind(thisContext, ...args)()
+}
 
+/**
+ * Applies an object as a thisContext and additional arguments to the callback function. Args are supplied argument by argument, separated by comma.
+ * @param {Function} thisContext the item to be bound as the return function's `this` keyword
+ * @param {any} args the remaining arguments to be passed down to the callback
+ * @returns {any} the return value of the callback function, given thisContext and args
+*/
+Function.prototype.myCall = function(thisContext, ...args) {
+  return this.myBind(thisContext, ...args)()
+}
+
+// test object for this binding
 const obj = { num: 0, c: function () { console.log(this) } }
 
-function logNums(x, y) { console.log(`x: ${x}, y: ${y}`) }
+// functions for method testing
+function logNums(x, y) {
+  console.log(`this.num: ${this.num}, x: ${x}, y: ${y}`)
+}
 function callback() {
   console.log('tt', this)
   return Object.keys(this).length
 }
 
-
-const anotherBound = callback.myBind(obj)
-b = anotherBound()
-console.log(b)
-
-const bound = logNums.myBind(obj, 1)
-bound(2)
-
-
-// console.log(callback.myCall(obj))
+// testing methods
+logNums.myApply(obj, [3, 4])
+logNums.myCall(obj, 5, 6)
